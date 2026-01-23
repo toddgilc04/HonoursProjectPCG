5c68b4746a229a3ddc852431ead95aba15529b4a
toddgilc <toddgilc@gmail.com>
2026-01-23 15:03:09 +0000

    Reading and writing to json

--- Content/Python/hellotest.py
+++ Content/Python/hellotest.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fdffbfeb1696d3e756ba046fa797f5da97ca2ae32a46de430206297834de6c3
-size 588
+oid sha256:9436094bc96ee6a9f0867af7dc08a2ded5677450733613d9b125aa143c9c8ee9
+size 1063
