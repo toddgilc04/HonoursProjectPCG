dc9121b8b34701d4e87225a5fbec6dc31a10ea91
toddgilc <toddgilc@gmail.com>
2026-02-03 22:16:46 +0000

    New downsample formula

--- Python/analyseAudio.py
+++ Python/analyseAudio.py
@@ -59,8 +59,6 @@ beats = librosa.util.fix_frames(beats)
 beatStrengths = onsetEnvHigherCap[beats]
 beatStrengthsNormalise = (beatStrengths - numpy.min(beatStrengths)) / (numpy.max(beatStrengths) - numpy.min(beatStrengths))
 # this gives the beat strengths from 0to 1 per onset beat, next normalise it so it can be applied to spline points in ue5
-
-
 beatStrengthsList = beatStrengthsNormalise.tolist()
 beatStrengthMean = float(beatStrengths.mean())
 print ('Beat Strength Mean: ' + format(beatStrengthMean))
